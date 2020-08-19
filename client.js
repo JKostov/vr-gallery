fa00294ec01e2dcdea37d1b1d92391fd7d33caf6
JKostov <julijekos@gmail.com>
2020-08-19 16:14:58 +0200

    Changed movement logic

--- client.js
+++ client.js
@@ -1,11 +1,13 @@
 
 import { ReactInstance, Location, Surface, Module } from 'react-360-web';
-import KeyboardCameraController from './controllers/keyboard-camera-controller';
+import { Vector3, Quaternion } from 'three';
+
+let r360 = null;
 
 function init(bundle, parent, options = {}) {
-  const r360 = new ReactInstance(bundle, parent, {
+  r360 = new ReactInstance(bundle, parent, {
     fullScreen: true,
-    nativeModules: [new surfaceModule()],
+    nativeModules: [new surfaceModule(), ctx => new MyModule(ctx),],
     ...options,
   });
 
@@ -21,12 +23,130 @@ function init(bundle, parent, options = {}) {
 
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
 
-  r360.controls.addCameraController(new KeyboardCameraController(-9.3, 3, -13, 3.8));
-
   // Hover image
   hoverImage = new Surface(500, 500, Surface.SurfaceShape.Flat);
   hoverImage.setAngle(-Math.PI / 2, 0);
   r360.renderToSurface(r360.createRoot("HoverImage", {}), hoverImage);
+}
+
+class ObjectNotation {
+  position = null;
+  quaternion = null;
+
+  constructor(position, quaternion) {
+    this.position = position;
+    this.quaternion = quaternion;
+  }
+
+  translateOnAxis = (axis, distance) => {
+    const v1 = new Vector3();
+
+    v1.copy(axis).applyQuaternion(this.quaternion);
+
+    this.position.add(v1.multiplyScalar(distance));
+  };
+
+  translateX = (distance) => {
+    this.translateOnAxis(new Vector3(1, 0, 0), distance);
+  };
+  translateY = (distance) => {
+    this.translateOnAxis(new Vector3(0, 1, 0), distance);
+  };
+  translateZ = (distance) => {
+    this.translateOnAxis(new Vector3(0, 0, 1), distance);
+  };
+}
+
+class MyModule extends Module {
+  x = 0;
+  z = 0;
+  movingX = 0;
+  movingZ = 0;
+  movingSpeed = 0.2;
+  xUpLimit = 9.3;
+  xDownLimit = -3;
+  zUpLimit = 13;
+  zDownLimit = -3.8;
+
+  constructor(ctx) {
+    super('MyModule');
+
+    this._ctx = ctx;
+
+    console.log(ctx);
+
+    document.addEventListener('keydown', (event) => this.onKeyDown(event));
+  }
+
+  getValueWithCallback(id) {
+    console.log(this.x, this.z);
+
+    const cameraQuaternion = r360.getCameraQuaternion();
+    const quaternion = new Quaternion(cameraQuaternion[0], cameraQuaternion[1], cameraQuaternion[2], cameraQuaternion[3]);
+    const position = new Vector3(this.x, 0, this.z);
+
+    const cameraObjectNotation = new ObjectNotation(position, quaternion);
+
+    if (this.movingZ !== 0) {
+      cameraObjectNotation.translateZ(this.movingZ);
+
+      this.movingZ = 0;
+    }
+
+    if (this.movingX !== 0) {
+      cameraObjectNotation.translateX(this.movingX);
+
+      this.movingX = 0;
+    }
+
+    if (cameraObjectNotation.position.z + this.movingZ < this.zDownLimit || cameraObjectNotation.position.z + this.movingZ > this.zUpLimit) {
+      return false;
+    }
+
+    if (cameraObjectNotation.position.x + this.movingX < this.xDownLimit || cameraObjectNotation.position.x + this.movingX > this.xUpLimit) {
+      return false;
+    }
+
+    this.x = cameraObjectNotation.position.x;
+    this.z = cameraObjectNotation.position.z;
+
+
+    this._ctx.invokeCallback(
+      id, // callback id, passed to the method
+      [this.x, this.z] // array of arguments passed to callback
+    );
+  }
+
+  onKeyDown(event) {
+    if (event.keyCode === 38 || event.keyCode === 87) {
+      this.moveForward();
+    }
+    else if (event.keyCode === 40 || event.keyCode === 83) {
+      this.moveBackward();
+    }
+    else if (event.keyCode === 37 || event.keyCode === 65) {
+      this.moveLeft();
+    }
+    else if (event.keyCode === 39 || event.keyCode === 68) {
+      this.moveRight();
+    }
+  }
+
+  moveForward() {
+    this.movingZ = this.movingSpeed;
+  }
+
+  moveBackward() {
+    this.movingZ = -this.movingSpeed;
+  }
+
+  moveLeft() {
+    this.movingX = this.movingSpeed;
+  }
+
+  moveRight() {
+    this.movingX = -this.movingSpeed;
+  }
 }
 
 class surfaceModule extends Module {
--- components/Gallery/index.js
+++ components/Gallery/index.js
@@ -2,47 +2,68 @@ import React, { Component } from 'react';
 import {
   View,
   asset,
+  NativeModules,
 } from 'react-360';
 import Entity from 'Entity';
 import Picture from '../Picture';
 
+const { MyModule } = NativeModules;
+
 export default class Gallery extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      x: 0,
+      z: 0,
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.callCheck(), 50);
+  }
+
+  callCheck() {
+    MyModule.getValueWithCallback((x, z) => this.setState({ x, z }));
+    setTimeout(() => this.callCheck(), 50);
+  }
 
   render() {
     return (
       <View>
         <Entity
-          style={{ transform: [{ scale: 1 }, { translate: [0, 0, 0] }] }}
+          style={{ transform: [{ scale: 1 }, { translate: [this.state.x, 0, this.state.z] }] }}
           source={{
             gltf2: asset('scene.gltf'),
           }}
         />
 
-        <Picture x={2.57} y={1.4} z={-11.4} height={1.4} width={1.4} rotateY={-90} asset='mona-lisa.jpg' />
-        <Picture x={2.57} y={2.8} z={-8.2} height={1.4} width={1.4} rotateY={-90} asset='' />
-        <Picture x={2.41} y={4.78} z={-4.64} height={2.5} width={1.7} rotateY={-90} asset='' />
-        <Picture x={2.35} y={6.9} z={-0.64} height={1.8} width={1.8} rotateY={-90} asset='' />
-        <Picture x={2.35} y={8.72} z={2.94} height={1.8} width={1.8} rotateY={-90} asset='' />
+        <Picture x={2.57 + this.state.x} y={1.4} z={-11.4 + this.state.z} height={1.4} width={1.4} rotateY={-90} asset='mona-lisa.jpg' />
+        <Picture x={2.57 + this.state.x} y={2.8} z={-8.2 + this.state.z} height={1.4} width={1.4} rotateY={-90} asset='' />
+        <Picture x={2.41 + this.state.x} y={4.78} z={-4.64 + this.state.z} height={2.5} width={1.7} rotateY={-90} asset='' />
+        <Picture x={2.35 + this.state.x} y={6.9} z={-0.64 + this.state.z} height={1.8} width={1.8} rotateY={-90} asset='' />
+        <Picture x={2.35 + this.state.x} y={8.72} z={2.94 + this.state.z} height={1.8} width={1.8} rotateY={-90} asset='' />
 
-        <Picture x={-3.93} y={10.62} z={3.98} height={1.72} width={1.34} rotateY={90} asset='' />
-        <Picture x={-3.93} y={12.34} z={1.8} height={1.72} width={1.34} rotateY={90} asset='' />
-        <Picture x={-3.93} y={14.05} z={-2.99} height={1.7} width={1.34} rotateY={90} asset='' />
-        <Picture x={-3.93} y={15.76} z={-4.64} height={1.7} width={1.34} rotateY={90} asset='' />
-        <Picture x={-3.93} y={17.46} z={-6.37} height={1.7} width={1.34} rotateY={90} asset='' />
-        <Picture x={-4.8} y={19.15} z={-12.38} height={1.7} width={3.08} rotateY={90} asset='' />
+        <Picture x={-3.93 + this.state.x} y={10.62} z={3.98 + this.state.z} height={1.72} width={1.34} rotateY={90} asset='' />
+        <Picture x={-3.93 + this.state.x} y={12.34} z={1.8 + this.state.z} height={1.72} width={1.34} rotateY={90} asset='' />
+        <Picture x={-3.93 + this.state.x} y={14.05} z={-2.99 + this.state.z} height={1.7} width={1.34} rotateY={90} asset='' />
+        <Picture x={-3.93 + this.state.x} y={15.76} z={-4.64 + this.state.z} height={1.7} width={1.34} rotateY={90} asset='' />
+        <Picture x={-3.93 + this.state.x} y={17.46} z={-6.37 + this.state.z} height={1.7} width={1.34} rotateY={90} asset='' />
+        <Picture x={-4.8 + this.state.x} y={19.15} z={-12.38 + this.state.z} height={1.7} width={3.08} rotateY={90} asset='' />
 
-        <Picture x={-5.25} y={20.85} z={-12.38} height={1.7} width={3.08} rotateY={-90} asset='' />
-        <Picture x={-4.2} y={22.34} z={-6.38} height={1.3} width={1} rotateY={-90} asset='' />
-        <Picture x={-4.2} y={23.64} z={-2.97} height={1.3} width={1} rotateY={-90} asset='' />
-        <Picture x={-4.41} y={25.22} z={-4.66} height={1.8} width={1.4} rotateY={-90} asset='' />
-        <Picture x={-4.41} y={27} z={1.8} height={1.8} width={1.4} rotateY={-90} asset='' />
-        <Picture x={-4.41} y={28.8} z={3.97} height={1.8} width={1.4} rotateY={-90} asset='' />
+        <Picture x={-5.25 + this.state.x} y={20.85} z={-12.38 + this.state.z} height={1.7} width={3.08} rotateY={-90} asset='' />
+        <Picture x={-4.2 + this.state.x} y={22.34} z={-6.38 + this.state.z} height={1.3} width={1} rotateY={-90} asset='' />
+        <Picture x={-4.2 + this.state.x} y={23.64} z={-2.97 + this.state.z} height={1.3} width={1} rotateY={-90} asset='' />
+        <Picture x={-4.41 + this.state.x} y={25.22} z={-4.66 + this.state.z} height={1.8} width={1.4} rotateY={-90} asset='' />
+        <Picture x={-4.41 + this.state.x} y={27} z={1.8 + this.state.z} height={1.8} width={1.4} rotateY={-90} asset='' />
+        <Picture x={-4.41 + this.state.x} y={28.8} z={3.97 + this.state.z} height={1.8} width={1.4} rotateY={-90} asset='' />
 
-        <Picture x={-10.61} y={30.26} z={2.64} height={1.4} width={1.4} rotateY={90} asset='' />
-        <Picture x={-10.61} y={31.66} z={-0.58} height={1.4} width={1.4} rotateY={90} asset='' />
-        <Picture x={-10.75} y={33.66} z={-4.13} height={2.6} width={1.7} rotateY={90} asset='' />
-        <Picture x={-10.79} y={35.86} z={-8.14} height={1.8} width={1.8} rotateY={90} asset='' />
-        <Picture x={-10.79} y={37.66} z={-11.72} height={1.8} width={1.8} rotateY={90} asset='' />
+        <Picture x={-10.61 + this.state.x} y={30.26} z={2.64 + this.state.z} height={1.4} width={1.4} rotateY={90} asset='' />
+        <Picture x={-10.61 + this.state.x} y={31.66} z={-0.58 + this.state.z} height={1.4} width={1.4} rotateY={90} asset='' />
+        <Picture x={-10.75 + this.state.x} y={33.66} z={-4.13 + this.state.z} height={2.6} width={1.7} rotateY={90} asset='' />
+        <Picture x={-10.79 + this.state.x} y={35.86} z={-8.14 + this.state.z} height={1.8} width={1.8} rotateY={90} asset='' />
+        <Picture x={-10.79 + this.state.x} y={37.66} z={-11.72 + this.state.z} height={1.8} width={1.8} rotateY={90} asset='' />
       </View>
     );
   }
