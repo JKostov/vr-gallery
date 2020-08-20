39b1a989ae81a9beacb328b8ca1bab8d34581cce
JKostov <julijekos@gmail.com>
2020-08-20 15:59:29 +0200

    Explained image added

--- client.js
+++ client.js
@@ -5,28 +5,42 @@ import { Vector3, Quaternion } from 'three';
 let r360 = null;
 
 function init(bundle, parent, options = {}) {
+
+  // Explained image surface
+  explainedImage = new Surface(500, 500, Surface.SurfaceShape.Flat);
+
   r360 = new ReactInstance(bundle, parent, {
     fullScreen: true,
     nativeModules: [new surfaceModule(), ctx => new MyModule(ctx),],
+    frame: () => {
+      const cameraQuaternion = r360.getCameraQuaternion();
+      const quaternion = new Quaternion(cameraQuaternion[0], cameraQuaternion[1], cameraQuaternion[2], cameraQuaternion[3]);
+
+      const v1 = new Vector3(0, 0, -1);
+      v1.applyQuaternion(quaternion);
+
+      const cx = v1.x;
+      const cy = v1.y;
+      const cz = v1.z;
+
+      const horizAngle = Math.atan2(cx, -cz);
+      const vertAngle = Math.asin(cy / Math.sqrt(cx * cx + cy * cy + cz * cz));
+
+      // Custom surface follow camera
+      explainedImage.setAngle(horizAngle, vertAngle);
+    },
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
-  r360.renderToSurface(
-    r360.createRoot('vr_gallery', {}),
-    r360.getDefaultSurface()
-  );
-
   const customLocation = new Location([0, -1, 0]);
-
   r360.renderToLocation(r360.createRoot('vr_gallery'), customLocation);
-
   r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
 
-  // Hover image
+  // Custom surfaces
   hoverImage = new Surface(500, 500, Surface.SurfaceShape.Flat);
   hoverImage.setAngle(-Math.PI / 2, 0);
   r360.renderToSurface(r360.createRoot("HoverImage", {}), hoverImage);
+  r360.renderToSurface(r360.createRoot("ExplainedImage", {}), explainedImage);
 }
 
 class ObjectNotation {
